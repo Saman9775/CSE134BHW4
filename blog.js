68f9b27b7e4b1c14f37360b2f25a61f3148fede4
Saman9775 <s1zarei@ucsd.edu>
2022-07-27 15:04:14 -0700

    let's sqweqqwe

--- blog.js
+++ blog.js
@@ -2,3 +2,8 @@ export function open_add(){
     document.getElementById("dialog_info").show();
 }
 document.getElementById("addSong").addEventListener("click", open_add);
+
+export function close_add(){
+    document.getElementById("dialog_info").close();
+}
+document.getElementById("cancel_but").addEventListener("click", close_add);
