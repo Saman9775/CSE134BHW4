dd3f93ac4a12e8a706958a2bdddd4a3996b4d42b
Saman9775 <s1zarei@ucsd.edu>
2022-07-27 16:10:40 -0700

    let's sqweqqweqwe

--- blog.js
+++ blog.js
@@ -3,7 +3,38 @@ export function open_add(){
 }
 document.getElementById("addSong").addEventListener("click", open_add);
 
-export function close_add(){
+export function close_add_cancel(){
     document.getElementById("dialog_info").close();
 }
-document.getElementById("cancel_but").addEventListener("click", close_add);
+document.getElementById("cancel_but").addEventListener("click", close_add_cancel);
+
+export function add_item(something){
+
+    let newItem;
+    newItem = document.createElement(`li`);
+    newItem.innerHTML = something;
+    document.getElementById("create_item").appendChild(newItem);
+}
+
+export function save_added_item(){
+
+    let singerName = document.getElementById("singer_name").value;
+    let birthYear = document.getElementById("singer_year").value;
+    let songName = document.getElementById("singer_summery").value;
+
+    // Nothing Should be null or empty
+
+    if((singerName == null || singerName == "") || (birthYear == null || birthYear == "") || (songName == null || songName == "")){
+        
+        alert("ERROR: Cehck your inputs ");
+    }
+
+    else{
+
+        add_item(`Title(Singer): ${singerName}, Date(Birthday Year): ${birthYear}, Summery(Song Name): ${songName}`);
+    }
+
+    document.getElementById("dialog_info").close();
+}
+
+document.getElementById("ok_but").addEventListener("click", save_added_item);
