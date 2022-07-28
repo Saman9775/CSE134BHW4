5e7c2b9b50ae98514ae0ed748fe105aaf2614e96
Saman9775 <s1zarei@ucsd.edu>
2022-07-27 19:51:21 -0700

    remoqweeqwwereqweqw

--- blog.js
+++ blog.js
@@ -74,12 +74,12 @@ export function remove_item(){
     
     for(let i = 0; i < document.getElementById("create_item").getElementsByClassName("forDelete").length; i++){
 
-        document.getElementById("create_item").getElementsByClassName("forDelete")[i] = function(){
+        document.getElementById("create_item").getElementsByClassName("forDelete")[i].addEventListener("click", save_added_item) = function(){
 
             remove_prepare(this);
             localStorage.setItem(`list_data`,JSON.stringify(document.getElementById("create_item").innerHTML));
         }
     }
 }
-document.getElementsByClassName("forDelete").addEventListener("click", remove_item);
+
 
